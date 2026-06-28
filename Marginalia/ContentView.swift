6029675db7c1dba2184a6f7d5dd91e7981256005
Anirban <anirbanphukan7@gmail.com>
2026-06-28 00:52:25 -0400

    Initial Commit

---
+++ Marginalia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Marginalia
+//
+//  Created by Anirban Phukan on 6/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Marginalia/MarginaliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarginaliaApp.swift
+//  Marginalia
+//
+//  Created by Anirban Phukan on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MarginaliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
